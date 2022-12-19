4805cfd86b9fff63f8389ed91952ecdd890102f8
AlbertUlysses <Albert.Chavez-non-empl@moodys.com>
2022-12-19 10:51:54 -0800

    update minigrep

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,88 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub case_sensitive: bool,
 }
 impl Config {
 
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() != 3 {
-            return Err("Not Enough Arguments.");
-        }
-        let query = &args[1];
-        let file_path = &args[2];
-        
-        Ok(Config {query: query.to_string(),file_path:  file_path.to_string()})
-    }
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
 
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string."),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name."),
+        };
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { query, file_path, case_sensitive })
+    }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{}", contents);
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines()
+        .filter(|line| line.to_ascii_lowercase().contains(&query.to_ascii_lowercase()))
+        .collect()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Duct tape.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,13 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In the file {}", config.file_path);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Problem reading the file: {}", e);
+        eprintln!("Problem reading the file: {}", e);
         process::exit(1);
     };
 
